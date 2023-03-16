5e7f55878c35bf54fb4cb364de9e3f7124971089
ritik2102 <ritik21feb@gmail.com>
2023-03-16 16:34:25 +0530

    meta and OGP

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {Header} from '../components/header';
+import {Nav} from '../components/nav';
+import {Main} from '../components/main';
+import {Footer} from  '../components/footer';
 
 function App() {
   return (
-    <div className="App">
+    <>
+      <Header/>
+      <Nav/>
+      <Main/>
+      <Footer/>
       Homepage
-    </div>
+    </>
   );
 }
 
